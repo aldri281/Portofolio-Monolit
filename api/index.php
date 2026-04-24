c0f73c8c254695facdb4fd00848017ba76a23d71
aldri281 <aldri.hardiansyah@gmail.com>
2026-04-24 11:32:41 +0700

    fix: suppress deprecated notices from vendor in production entry point

--- api/index.php
+++ api/index.php
@@ -1,2 +1,6 @@
 <?php
+
+// Suppress deprecated notices from vendor/framework files in production
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+
 require __DIR__ . '/../public/index.php';
